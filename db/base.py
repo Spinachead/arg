082d6a4f66ae81a85b36537bd5a3e829c2cb05b9
spinachead <2607771759@qq.com>
2025-12-25 22:24:55 +0800

    info.db路径错误问题

--- db/base.py
+++ db/base.py
@@ -4,8 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from settings import Settings
+
 engine = create_engine(
-    "sqlite:///" + str("knowledge_base/info.db"),
+    Settings.basic_settings.SQLALCHEMY_DATABASE_URI,
     json_serializer=lambda obj: json.dumps(obj, ensure_ascii=False),
 )
 
