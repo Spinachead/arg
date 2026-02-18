ac8dbfa607f3019f8565ee1e837c2dfe10de6be9
spinachead <2607771759@qq.com>
2026-02-19 00:05:54 +0800

    response节点

--- app/core/state_graph/nodes/main_graph/respond.py
+++ app/core/state_graph/nodes/main_graph/respond.py
@@ -4,21 +4,14 @@ from langchain.chat_models import init_chat_model
 from langchain_core.runnables import RunnableConfig
 from langchain_core.messages import BaseMessage
 from settings import Settings
-
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 
 async def respond(
     state: AgentState, *, config: RunnableConfig
 ) -> dict[str, list[BaseMessage]]:
     """
-    Generates a final response to the user based on the agent's accumulated knowledge and messages.
-
-    Args:
-        state (AgentState): The current state of the agent, including knowledge and messages.
-        config (RunnableConfig): Configuration for the runnable execution.
-
-    Returns:
-        dict[str, list[BaseMessage]]: A dictionary containing the generated response message(s).
+    根据上下文调用LLM大模型就行回复，支持工具调用
     """
 
     model = init_chat_model(
@@ -29,9 +22,15 @@ async def respond(
         openai_api_base=Settings.app_settings.openai_api_base,
         openai_api_key=Settings.app_settings.openai_api_key,
     )
-    prompt = RESPONSE_SYSTEM_PROMPT.format(context=state.context)
-    messages = [{"role": "system", "content": prompt}] + state.messages
-    response = await model.ainvoke(messages, config)
-    print(f"respond:{response}")
 
+    chat_prompt = ChatPromptTemplate.from_messages([
+        ("system", RESPONSE_SYSTEM_PROMPT),
+        MessagesPlaceholder(variable_name="history")
+    ])
+    chain = chat_prompt | model
+    response = await chain.ainvoke({
+        "history": state.messages,
+        "context": state.context,
+    }, config)
+    print(f"respond:{response}")
     return {"messages": [response]}
